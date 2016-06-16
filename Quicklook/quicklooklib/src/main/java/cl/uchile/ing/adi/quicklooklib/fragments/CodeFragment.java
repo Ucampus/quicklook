aeca30dae877c2de94f67bfcec7cfa546a452d47
adderou <dudu@adderou.cl>
2016-06-16 18:01:09 -0400

    Adding loading screen to CodeView

--- Quicklook/quicklooklib/src/main/java/cl/uchile/ing/adi/quicklooklib/fragments/CodeFragment.java
+++ Quicklook/quicklooklib/src/main/java/cl/uchile/ing/adi/quicklooklib/fragments/CodeFragment.java
@@ -1,9 +1,11 @@
 package cl.uchile.ing.adi.quicklooklib.fragments;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
@@ -20,13 +22,17 @@ import cl.uchile.ing.adi.quicklooklib.R;
  */
 public class CodeFragment extends QuicklookFragment {
 
+    private WebView web;
+    private ProgressDialog pd;
+
     @Override
     public View createItemView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v =  inflater.inflate(R.layout.fragment_web, container, false);
-        WebView web = (WebView) v.findViewById(R.id.web_fragment);
+        web = (WebView) v.findViewById(R.id.web_fragment);
         WebSettings ws = web.getSettings();
-
+        pd = ProgressDialog.show(getActivity(),
+                getString(R.string.quicklook_loading_file),"Loading");
         ws.setBuiltInZoomControls(true);
         ws.setJavaScriptEnabled(true);
         //ws.setLoadWithOverviewMode(true);
@@ -47,12 +53,26 @@ public class CodeFragment extends QuicklookFragment {
             is.close();
             content += "<pre><code>"+c.replace( "<", "&lt;" )+"</code></pre>";
         } catch (IOException ignored)  {}
-        content += "<script>hljs.initHighlightingOnLoad();</script>";
+        content += "<script>(function() { hljs.initHighlighting();window.androidFunctions.dismissProgressDialog()})();</script>";
         content += "</body></html>";
-
         web.loadDataWithBaseURL("file:///android_asset/", content, "text/html", "UTF-8", null);
+        //Add a JavaScriptInterface, so I can make calls from the web to Java methods
+        web.addJavascriptInterface(new WebPageListener(), "androidFunctions");
+
 
         return v;
+    }
+
+    public class WebPageListener {
+        @JavascriptInterface
+        public void dismissProgressDialog(){
+           getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    pd.dismiss();
+                }
+            });
+        }
     }
 
 }
